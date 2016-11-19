1a3b4857026fcccfa027bd4bf7d186f05d6874a0
prajneya <prajneya@gmail.com>
2016-11-19 18:59:13 +0530

    Update covering.cpp

--- covering.cpp
+++ covering.cpp
@@ -1,30 +1,34 @@
 #include<iostream>
 #include<vector>
+#include<utility>
 #include<algorithm>
 using namespace std;
 
-
-
 int main(){
-	long long count[5100] = {0};
 	long long n;
 	cin >> n;
-	vector<long long> in;
-	for(long long i=0; i<n; i++){
-		long long x, y;
-		cin >> x >> y;
-		for(long long j=x; j<=y; j++){
-			in.push_back(j);
+	vector<int> ans;
+	pair<long long, long long> in[n];
+	for(int i=0; i<n; i++){
+		cin >> in[i].first >> in[i].second;
+	}
+	sort(in, in+n);
+	for(int i=0; i<n; i++){
+	
+	}
+	long long tempcount=0;
+	ans.push_back(in[0].second);
+	for(long long i=1; i<n; i++){
+		for(long long j=0; j<ans.size(); j++){
+			if(ans[j]>=in[i].first && ans[j]<=in[i].second){
+				tempcount++;
+			}
 		}
+		if(tempcount==0){
+			ans.push_back(in[i].second);
+		}
+		tempcount = 0;
 	}
-	
-	for(long long i=0; i<in.size(); i++){
-		count[in[i]] = count[in[i]]+1;
-	}
-	
-	long long ans;
-	ans = *max_element(count, count+5100);
-	
-	cout << (n-ans)+1;
-	
+
+	cout << ans.size();
 }
